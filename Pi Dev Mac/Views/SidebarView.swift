b15291f6881b4795a5fe6af7ce7b0e49fac87ee0
thisisommore <ommore501@gmail.com>
2026-08-10 10:22:52 +0530

    Set scrollerInsets left -10, right 0

--- Pi Dev Mac/Views/SidebarView.swift
+++ Pi Dev Mac/Views/SidebarView.swift
@@ -231,7 +231,7 @@ private struct ScrollViewStyleConfigurator: NSViewRepresentable {
             scrollView.scrollerStyle = .overlay
             scrollView.automaticallyAdjustsContentInsets = false
             scrollView.contentInsets = NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-            scrollView.scrollerInsets = NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 14)
+            scrollView.scrollerInsets = NSEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
             if !(scrollView.verticalScroller is SidebarScroller) {
                 let scroller = SidebarScroller()
                 scroller.controlSize = .small
