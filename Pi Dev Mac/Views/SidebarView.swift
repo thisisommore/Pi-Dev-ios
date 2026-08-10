b15e11476675696dbd25512c7bcdf6f954043736
thisisommore <ommore501@gmail.com>
2026-08-10 10:10:42 +0530

    Test trailing -16 with scrollbar

--- Pi Dev Mac/Views/SidebarView.swift
+++ Pi Dev Mac/Views/SidebarView.swift
@@ -109,7 +109,7 @@ struct SidebarView: View {
             .padding(.bottom, 8)
             .background(ScrollViewStyleConfigurator(hasScrollbar: $hasScrollbar))
         }
-        .contentMargins(.trailing, hasScrollbar ? -4 : 0, for: .scrollContent)
+        .contentMargins(.trailing, hasScrollbar ? -16 : 0, for: .scrollContent)
         .scrollContentBackground(.hidden)
         .background(Color.clear)
     }
